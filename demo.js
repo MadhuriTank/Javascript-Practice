0c5e60c116cd56492f81fd4889d81d65d59c26ae
Madhuri Tank <madhuritank10@gmail.com>
2021-06-14 21:26:53 +0530

    closure change

--- demo.js
+++ demo.js
@@ -4,4 +4,15 @@ console.log("Fisrt hoisting program");
 
 console.log("Accessing x before initializing it : " + x);
 
+function a(){
+	console.log("x inside a : " + x);
+	console.log("putting y to test closure" + y);
+	b();
+	function b(){
+		console.log("x inside b : "+x);
+		console.log("y inside b : "+y);
+	}
+}
+
 var x = 10;
+a();
